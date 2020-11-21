59d6d21d9c51349d9ac713fba93dfce208161fdf
ryan ernst <onlyhouse@gmail.com>
2020-11-21 16:40:12 +0100

    clean

--- src/pages/executive.js
+++ src/pages/executive.js
@@ -11,10 +11,10 @@ const ExecutivePage = () => (
     </div>
     <div className="div-block">
       <div className= {styles.wContainer}><img src="images/team-1.jpg" loading="lazy" width="300" sizes="(max-width: 479px) 150.40625px, 300px" srcSet="images/team-1-p-800.jpeg 800w, images/team-1-p-1080.jpeg 1080w, images/team-1.jpg 1524w" alt="" className="image-3" />
-        <div className={styles.textbox}>Jim Farrell<br />President</div>
+        <div className={styles.textbox}><p>Jim Farrell<br />President</p></div>
       </div>
       <div className= {styles.wContainer}><img src="images/team-2.jpg" loading="lazy" width="300" alt="" className="image-4" />
-        <div className={styles.textbox}>Erika Aust<br />Vice President</div>
+        <div className={styles.textbox}><p>Erika Aust<br />Vice President</p></div>
       </div>
       <div className= {styles.wContainer}><img src="images/team-3.jpg" loading="lazy" width="300" sizes="(max-width: 479px) 255.765625px, 300px" srcSet="images/team-3-p-500.jpeg 500w, images/team-3.jpg 1524w" alt="" className="image-5" />
         <div className={styles.textbox}>James Loder<br />Secretary/Treasurer</div>
