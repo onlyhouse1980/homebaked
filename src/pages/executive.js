93042a5b2a6c62c7591cb3851be77246fb20b8d4
ryan ernst <onlyhouse@gmail.com>
2020-11-21 17:04:05 +0100

    more

--- src/pages/executive.js
+++ src/pages/executive.js
@@ -9,7 +9,7 @@ const ExecutivePage = () => (
     <h2 className='text-center'>OBCG<br />Executive Board</h2>
     <p></p>
     </div>
-    <div className="div-block">
+    <div className={styles.divBlock}>
       <div className= {styles.wContainer}><img src="images/team-1.jpg" loading="lazy" width="300" sizes="(max-width: 479px) 150.40625px, 300px" srcSet="images/team-1-p-800.jpeg 800w, images/team-1-p-1080.jpeg 1080w, images/team-1.jpg 1524w" alt="" className="image-3" />
         <div className={styles.textbox}><p>Jim Farrell<br />President</p></div>
       </div>
