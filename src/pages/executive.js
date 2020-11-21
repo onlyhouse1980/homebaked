aab01858516a7dc759e7c55e9a60fb47d7acbb2d
ryan ernst <onlyhouse@gmail.com>
2020-11-21 17:41:05 +0100

    ..

--- src/pages/executive.js
+++ src/pages/executive.js
@@ -10,14 +10,14 @@ const ExecutivePage = () => (
     <p></p>
     </div>
     <div className={styles.divBlock}>
-      <div className= {styles.wContainer}><img src="images/team-1.jpg" loading="lazy" width="300" sizes="(max-width: 479px) 150.40625px, 300px" srcSet="images/team-1-p-800.jpeg 800w, images/team-1-p-1080.jpeg 1080w, images/team-1.jpg 1524w" alt="" className="image-3" />
+      <div className= {styles.wContainer}><img src="images/team-1.jpg" loading="lazy" width="300" alt="Member Photo" className="image-3" />
         <div className={styles.textbox}><p>Jim Farrell<br />President</p></div>
       </div>
-      <div className= {styles.wContainer}><img src="images/team-2.jpg" loading="lazy" width="300" alt="" className="image-4" />
+      <div className= {styles.wContainer}><img src="images/team-2.jpg" loading="lazy" width="300" alt="Member Photo" className="image-4" />
         <div className={styles.textbox}><p>Erika Aust<br />Vice President</p></div>
       </div>
-      <div className= {styles.wContainer}><img src="images/team-3.jpg" loading="lazy" width="300" sizes="(max-width: 479px) 255.765625px, 300px" srcSet="images/team-3-p-500.jpeg 500w, images/team-3.jpg 1524w" alt="" className="image-5" />
-        <div className={styles.textbox}>James Loder<br />Secretary/Treasurer</div>
+      <div className= {styles.wContainer}><img src="images/team-3.jpg" loading="lazy" width="300" alt="Member Photo" className="image-5" />
+        <div className={styles.textbox}><p>James Loder<br />Secretary/Treasurer</p></div>
       </div>
     </div>
   </>
