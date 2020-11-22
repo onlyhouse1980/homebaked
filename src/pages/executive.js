66fc490f89fa144a927aa4de51d98a9f846c16f4
ryan ernst <onlyhouse@gmail.com>
2020-11-22 21:07:09 +0100

    exec

--- src/pages/executive.js
+++ src/pages/executive.js
@@ -6,17 +6,17 @@ const Executive = () => (
   <>
     <VideoBg1 /> 
     <div className={styles.textbox}>
-    <h2 className='text-center'>OBCG<br />Executive Board</h2>
+    <h2 className='text-center'><b>OBCG</b><br /><b>Executive Board</b></h2>
     <p></p>
     </div>
     <div className={styles.divBlock}>
-      <div className= {styles.wContainer}><img src="images/team-1.jpg" loading="lazy" width="300" alt="Member Photo" className={styles.Image5} />
+      <div className= {styles.content}><img src="images/team-1.jpg" loading="lazy" width="300" alt="Member Photo" className={styles.Image5} />
         <div className={styles.content}><p>Jim Farrell<br />President</p></div>
       </div>
-      <div className= {styles.wContainer}><img src="images/team-2.jpg" loading="lazy" width="300" alt="Member Photo" className={styles.Image5} />
+      <div className= {styles.content}><img src="images/team-2.jpg" loading="lazy" width="300" alt="Member Photo" className={styles.Image5} />
         <div className={styles.content}><p>Erika Aust<br />Vice President</p></div>
       </div>
-      <div className= {styles.wContainer}><img src="images/team-3.jpg" loading="lazy" width="300" alt="Member Photo" className={styles.Image5} />
+      <div className= {styles.content}><img src="images/team-3.jpg" loading="lazy" width="300" alt="Member Photo" className={styles.Image5} />
         <div className={styles.content}><p>James Loder<br />Secretary/Treasurer</p></div>
       </div>
     </div>
