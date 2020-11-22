f42c8d29030c29cd723f184b4e973d68d0c9be6f
ryan ernst <onlyhouse@gmail.com>
2020-11-22 16:11:54 +0100

    fixed

--- src/pages/executive.js
+++ src/pages/executive.js
@@ -1,6 +1,6 @@
 import ReactImage from '../assets/mdb-react-small.png';
 import VideoBg1 from '../components/VideoBg1';
-import styles from '../styles/Home.module.css';
+import styles from '../styles/executive.module.css';
 
 const Executive = () => (
   <>
@@ -10,14 +10,14 @@ const Executive = () => (
     <p></p>
     </div>
     <div className={styles.divBlock}>
-      <div className= {styles.wContainer}><img src="images/team-1.jpg" loading="lazy" width="300" alt="Member Photo" className={styles.image5} />
-        <div className={styles.textbox}><p>Jim Farrell<br />President</p></div>
+      <div className= {styles.wContainer}><img src="images/team-1.jpg" loading="lazy" width="300" alt="Member Photo" className={styles.Image5} />
+        <div className={styles.textbox1}><p>Jim Farrell<br />President</p></div>
       </div>
-      <div className= {styles.wContainer}><img src="images/team-2.jpg" loading="lazy" width="300" alt="Member Photo" className={styles.image5} />
-        <div className={styles.textbox}><p>Erika Aust<br />Vice President</p></div>
+      <div className= {styles.wContainer}><img src="images/team-2.jpg" loading="lazy" width="300" alt="Member Photo" className={styles.Image5} />
+        <div className={styles.textbox2}><p>Erika Aust<br />Vice President</p></div>
       </div>
-      <div className= {styles.wContainer}><img src="images/team-3.jpg" loading="lazy" width="300" alt="Member Photo" className={styles.image5} />
-        <div className={styles.textbox}><p>James Loder<br />Secretary/Treasurer</p></div>
+      <div className= {styles.wContainer}><img src="images/team-3.jpg" loading="lazy" width="300" alt="Member Photo" className={styles.Image5} />
+        <div className={styles.textbox3}><p>James Loder<br />Secretary/Treasurer</p></div>
       </div>
     </div>
   </>
