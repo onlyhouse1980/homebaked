b03f8187f27125d777a7e41170a28e37d0a4c6a2
ryan ernst <onlyhouse@gmail.com>
2020-11-22 09:20:54 +0100

    clear text
    on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
 import Carousel from '../components/Carousel';
 import ReactImage from '../assets/mdb-react-small.png';
 import VideoBg from '../components/VideoBg';
-import styles from '../styles/Home.module.css'
+import styles from '../styles/WelcomePage.module.css'
 
 const WelcomePage = () => (
   <>
     <VideoBg />
     <div className={styles.textbox}>
-      <h3 className='text-center'>Orchard Beach <br />Community Group</h3>
+      <h3 className={styles.textCenter}>Orchard Beach <br />Community Group</h3>
     </div>
     <Carousel />
   </>
