27cd102a0c428ffacb1efaccf3163571c76eeb32
ryan ernst <onlyhouse@gmail.com>
2020-11-24 22:53:40 +0100

    contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,7 +4,7 @@ const Contact = () => (
   <>
     <div className={styles.content}>
       <h2 className='textCenter'>Orchard Beach Established 1954</h2>
-      <img src="images/fullsmall.png" loading="lazy" height="" sizes="90vw" srcSet="images/fullsmall-p-500.png 500w, images/fullsmall-p-800.png 800w, images/fullsmall-p-1080.png 1080w, images/fullsmall-p-1600.png 1600w, images/fullsmall.png 1920w" alt="" className="lightbox-link" />
+      <img src="images/fullsmall.png" loading="lazy" height="" sizes="90vw" srcSet="images/fullsmall-p-500.png 500w, images/fullsmall-p-800.png 800w, images/fullsmall-p-1080.png 1080w, images/fullsmall-p-1600.png 1600w, images/fullsmall.png 1920w" alt="" />
       <div className="textCenter">
         <h3>OBCG</h3>
         <p>Orchard Beach<br /> Grapeview, WA 98546<br /><br />
