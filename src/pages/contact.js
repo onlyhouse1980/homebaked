228132c2cb5d408cd705e2d20c8a585ad897eb23
ryan ernst <onlyhouse@gmail.com>
2020-11-24 21:43:35 +0100

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import imageComponent from "../components/imageComponent";
+import "../components/ImageComponent";
 import VideoBg1 from '../components/VideoBg1';
 import styles from '../styles/contact.module.css'
 
 function contact() {
   return (
-    <div className="App">
+    <div className="Contact">
       <h1>Orchard Beach Community Group</h1>
       <h4>Some contact info.. </h4>
       <h5> mailto reference </h5>
@@ -15,5 +15,5 @@ function contact() {
   );
 }
 
-export default contact
+export default Contact
 
