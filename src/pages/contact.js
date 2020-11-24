e337ace6fe4a1c5bca4f1477230dfea52b35291f
ryan ernst <onlyhouse@gmail.com>
2020-11-24 20:34:24 +0100

    attempt

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,8 +7,9 @@ import styles from '../styles/contact.module.css'
 function contact() {
   return (
     <div className="App">
-      <h1>Modal popup</h1>
-      <h4>To trigger the popup click the picture, please! </h4>
+      <h1>Orchard Beach Community Group</h1>
+      <h4>Some contact info.. </h4>
+      <h5> mailto reference </h5>
       <ImageComponent />
     </div>
   );
