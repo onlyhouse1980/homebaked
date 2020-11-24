63ba206f26b16ba65e5d0d262669f07feec54fa0
ryan ernst <onlyhouse@gmail.com>
2020-11-24 23:05:26 +0100

    contact black

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,9 @@
-import styles from '../styles/About.module.css'
+import VideoBg1 from '../components/VideoBg1';
+import styles from '../styles/Contact.module.css'
 
 const Contact = () => (
   <>
+    <VideoBg1 />
     <div className={styles.content}>
       <h2 className='textCenter'>Orchard Beach Established 1954</h2>
       <img src="images/fullsmall.png" loading="lazy" height="" sizes="90vw" srcSet="images/fullsmall-p-500.png 500w, images/fullsmall-p-800.png 800w, images/fullsmall-p-1080.png 1080w, images/fullsmall-p-1600.png 1600w, images/fullsmall.png 1920w" alt="" />
