ba65bdcf17c47898438caf6c8f336f576baab88b
ryan ernst <onlyhouse@gmail.com>
2020-11-24 22:43:21 +0100

    Create contact.js

---
+++ src/pages/contact.js
@@ -0,0 +1,18 @@
+import styles from '../styles/About.module.css'
+
+const Contact = () => (
+  <>
+    <div className={styles.content}>
+      <h2 className='textCenter'>Orchard Beach Established 1954</h2>
+      <img src="images/fullsmall.png" loading="lazy" height="" sizes="90vw" srcSet="images/fullsmall-p-500.png 500w, images/fullsmall-p-800.png 800w, images/fullsmall-p-1080.png 1080w, images/fullsmall-p-1600.png 1600w, images/fullsmall.png 1920w" alt="" className="lightbox-link" />
+      <div className="textCenter">
+        <h3>OBCG</h3>
+        <p>Orchard Beach<br /> Grapeview, WA 98546<br /><br />
+          <strong>Email: </strong><a href="mailto:orchardwater@yahoo.com">orchardwater@yahoo.com</a>
+        </p>
+      </div>
+    </div>
+  </>
+);
+
+export default Contact;
