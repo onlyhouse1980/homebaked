b7713ee3374498182fc8fd4346adfa69445e37f4
ryan ernst <onlyhouse@gmail.com>
2020-11-27 13:13:18 +0100

    Update register.js

--- src/pages/register.js
+++ src/pages/register.js
@@ -22,41 +22,16 @@ const Example = () => (
           />
           <MDBCardBody>
             <form>
-              <p className='h5 text-center mb-4'>Sign up</p>
+              <p className='h5 text-center mb-4'>Check Your Usage</p>
               <div className='grey-text'>
                 <MDBInput
-                  label='Your name'
+                  label='Meter Serial #'
                   icon='user'
                   group
                   type='text'
                   validate
                   error='wrong'
                   success='right'
-                />
-                <MDBInput
-                  label='Your email'
-                  icon='envelope'
-                  group
-                  type='email'
-                  validate
-                  error='wrong'
-                  success='right'
-                />
-                <MDBInput
-                  label='Confirm your email'
-                  icon='exclamation-triangle'
-                  group
-                  type='text'
-                  validate
-                  error='wrong'
-                  success='right'
-                />
-                <MDBInput
-                  label='Your password'
-                  icon='lock'
-                  group
-                  type='password'
-                  validate
                 />
               </div>
               <div className='text-center'>
