81237dd712bcd125c23c744c13ce4a16ea7de0cb
ryan ernst <onlyhouse@gmail.com>
2020-12-03 17:33:45 +0100

    oops

--- src/components/YourReading.js
+++ src/components/YourReading.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { MDBInput } from "mdbreact";
 
-const InputPage = () => {
+const YourReading = () => {
   return (
     <MDBInput label="Your Reading" />
   );
--- src/pages/register.js
+++ src/pages/register.js
@@ -1,42 +1,10 @@
-import { useRouter } from 'next/router'
-import useSWR from 'swr'
-import UsageStepper from '../../components/UsageStepper'
+import UsageStepper from '../components/UsageStepper';
+import Link from 'next/link';
 
-const fetcher = async (url) => {
-  const res = await fetch(url)
-  const data = await res.json()
+const CheckUsage = () => (
+  <React.Fragment>
+    <UsageStepper />
+  </React.Fragment>
+);
 
-  if (res.status !== 200) {
-    throw new Error(data.message)
-  }
-  return data
-}
-
-export default function Person() {
-  const { query } = useRouter()
-  const { data, error } = useSWR(
-    () => query.id && `/api/people/${query.id}`,
-    fetcher
-  )
-
-  if (error) return <div>{error.message}</div>
-  if (!data) return <div>Loading...</div>
-
-  return (
-    <table>
-      <thead>
-        <tr>
-          <th>Meter Serial #</th>
-          <th>Reading on Oct 7, 2020</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>{data.id}</td>
-          <td>{data.Oct0720}</td>
-        </tr>
-      </tbody>
-      <UsageStepper />
-    </table>
-  )
-}
+export default CheckUsage;
