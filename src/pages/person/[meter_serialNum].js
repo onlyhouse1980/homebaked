0d88fe2728769d1442f504b2fd5381f874e966b5
ryan ernst <onlyhouse@gmail.com>
2020-12-12 23:24:20 +0100

    stuff

---
+++ src/pages/person/[meter_serialNum].js
@@ -0,0 +1,50 @@
+import { useRouter } from 'next/router'
+import useSWR from 'swr'
+
+const fetcher = async (url) => {
+  const res = await fetch(url)
+  const data = await res.json()
+
+  if (res.status !== 200) {
+    throw new Error(data.message)
+  }
+  return data
+}
+
+export default function Person() {
+  const { query } = useRouter()
+  const { data, error } = useSWR(
+    () => query.meter_serialNum && `/api/people/${query.meter_serialNum}`,
+    fetcher
+  )
+
+  if (error) return <div>{error.message}</div>
+  if (!data) return <div>Loading...</div>
+
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Serial Number</th>
+          <th></th>
+          <th>Jun 10th</th>
+          <th>August 10th</th>
+          <th>October 7th</th>
+          <th><input></input></th>
+          <th>Used</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{data.meter_serialNum}</td>
+          <td input>""</td>
+          <td>{data.june10_2020}</td>
+          <td>{data.aug10_2020}</td>
+          <td>{data.oct07_2020}</td>
+          <td input>""</td>
+          <td input>""</td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
